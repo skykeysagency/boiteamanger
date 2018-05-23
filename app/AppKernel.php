5ebe1fdaeb2831973f63e4e8c070885fcbe655da
didicluber <dylanouanounou@yahoo.fr>
2018-05-23 15:18:18 +0200

    add easyadmin bundle for backend

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new HWI\Bundle\OAuthBundle\HWIOAuthBundle(),
             new PUGX\AutocompleterBundle\PUGXAutocompleterBundle(),
             new Craue\FormFlowBundle\CraueFormFlowBundle(),
+            new EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
--- src/AppBundle/Form/PlatType.php
+++ src/AppBundle/Form/PlatType.php
@@ -14,6 +14,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
@@ -42,7 +43,7 @@ class PlatType extends AbstractType
                 'choice_label' => 'libelle',
                  'multiple' => true,
             ))
-            ->add('descriptionPlat', TextType::class, array(
+            ->add('descriptionPlat', TextareaType::class, array(
                 'label' => 'Description plat',
             ))
             ->add('imagePlat', FileType::class, array(
